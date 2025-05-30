15240ba58bd5343d8c722f6778cb8d5bd45aa85a
Matheus Torres <30413727+matttheust@users.noreply.github.com>
2025-05-30 08:02:22 -0300

    first commit

---
+++ MobileCoffeShop/MobileCoffeShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileCoffeShop
+//
+//  Created by Matheus  Torres on 30/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileCoffeShop/MobileCoffeShop/MobileCoffeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileCoffeShopApp.swift
+//  MobileCoffeShop
+//
+//  Created by Matheus  Torres on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MobileCoffeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
